bc6eb58d5435fcc300f2beeb7a71d20a592e9f75
zuokylin <zuokylin@gmail.com>
2017-06-12 15:22:12 +0800

    init dev branch

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// require less file
+require('./less/common.less')
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home-page">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'here is home page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
