7184ed3b64fe85883db0a67dbb5e2e093ff2bef8
zuokylin <zuokylin@gmail.com>
2017-06-14 18:08:08 +0800

    update

--- src/lib/util.js
+++ src/lib/util.js
@@ -7,3 +7,35 @@ export const colors = {
   purple: 'rgb(153, 102, 255)',
   grey: 'rgb(201, 203, 207)'
 }
+
+/**
+ * Provides requestAnimationFrame in a cross browser
+ * way.
+ */
+
+if (!window.requestAnimationFrame) {
+  window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+           window.webkitRequestAnimationFrame ||
+           window.mozRequestAnimationFrame ||
+           window.oRequestAnimationFrame ||
+           window.msRequestAnimationFrame ||
+           function (/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+             window.setTimeout(callback, 1000 / 60)
+           }
+  })()
+}
+
+/** * ERRATA: 'cancelRequestAnimationFrame' renamed to 'cancelAnimationFrame' to reflect an update to the W3C Animation-Timing Spec.
+ *
+ * Cancels an animation frame request.
+ * Checks for cross-browser support, falls back to clearTimeout.
+ * @param {number}  Animation frame request. */
+if (!window.cancelAnimationFrame) {
+  window.cancelAnimationFrame = (window.cancelRequestAnimationFrame ||
+                                 window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame ||
+                                 window.mozCancelAnimationFrame || window.mozCancelRequestAnimationFrame ||
+                                 window.msCancelAnimationFrame || window.msCancelRequestAnimationFrame ||
+                                 window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame ||
+                                 window.clearTimeout)
+}
--- src/lib/webgl.js
+++ src/lib/webgl.js
@@ -5,6 +5,7 @@
  */
 import { colors } from './util'
 import { initShaders } from './book'
+import { Matrix4 } from './matrix'
 console.log('colors', colors)
 export function main (canvas, config) {
   // 设置canvas尺寸
@@ -15,9 +16,11 @@ export function main (canvas, config) {
 
   let VSHADER_SOURCE = `
     attribute vec4 aPosition;
+    uniform vec4 uTranslation;
+    uniform mat4 uModelMatrix;
     attribute float aPointSize;
     void main() {
-      gl_Position = aPosition;
+      gl_Position = uModelMatrix * aPosition;
       gl_PointSize = aPointSize;
     }
   `
@@ -33,10 +36,16 @@ export function main (canvas, config) {
     return
   }
   let aPosition = gl.getAttribLocation(gl.program, 'aPosition')
+  let uTranslation = gl.getUniformLocation(gl.program, 'uTranslation')
+  let uModelMatrix = gl.getUniformLocation(gl.program, 'uModelMatrix')
   let aPointSize = gl.getAttribLocation(gl.program, 'aPointSize')
   let uFragColor = gl.getUniformLocation(gl.program, 'uFragColor')
   if (aPosition < 0) {
     console.log('Failed to get the storage location of a_Position')
+    return
+  }
+  if (uTranslation < 0) {
+    console.log('Failed to get the storage location of u_Translation')
     return
   }
   if (aPointSize < 0) {
@@ -47,53 +56,87 @@ export function main (canvas, config) {
     console.log('Failed to get the storage location of u_FragColor')
     return
   }
-  let gPoints = []
-  let gColors = []
-  canvas.addEventListener('mousedown', onMousedown, false)
-  function onMousedown (event) {
-    console.log('event', event)
-    let x = event.clientX
-    let y = event.clientY
-    let rect = event.target.getBoundingClientRect()
-    x = ((x - rect.left) - canvasSize.width / 2) / (canvasSize.width / 2)
-    y = (canvasSize.height / 2 - (y - rect.top)) / (canvasSize.height / 2)
-    gPoints.push({
-      x: x,
-      y: y
-    })
-    gColors.push([Math.random(), Math.random(), Math.random(), 1.0])
-    // 清空canvas
-    clear(gl)
-    gl.vertexAttrib1f(aPointSize, 50.0)
-    gPoints.forEach((d, i) => {
-      gl.vertexAttrib3f(aPosition, d.x, d.y, 0.0)
-      gl.uniform4f(uFragColor, gColors[i][0], gColors[i][1], gColors[i][2], gColors[i][3])
-      gl.drawArrays(gl.POINTS, 0, 1)
-    })
+  if (!uModelMatrix) {
+    console.log('Failed to get the storage location of uModelMatrix')
+    return
+  }
+  // let gPoints = []
+  // let gColors = []
+  // canvas.addEventListener('mousedown', onMousedown, false)
+  // function onMousedown (event) {
+  //   console.log('event', event)
+  //   let x = event.clientX
+  //   let y = event.clientY
+  //   let rect = event.target.getBoundingClientRect()
+  //   x = ((x - rect.left) - canvasSize.width / 2) / (canvasSize.width / 2)
+  //   y = (canvasSize.height / 2 - (y - rect.top)) / (canvasSize.height / 2)
+  //   gPoints.push({
+  //     x: x,
+  //     y: y
+  //   })
+  //   gColors.push([Math.random(), Math.random(), Math.random(), 1.0])
+  //   // 清空canvas
+  //   clear(gl)
+  //   gl.vertexAttrib1f(aPointSize, 50.0)
+  //   gPoints.forEach((d, i) => {
+  //     gl.vertexAttrib3f(aPosition, d.x, d.y, 0.0)
+  //     gl.uniform4f(uFragColor, gColors[i][0], gColors[i][1], gColors[i][2], gColors[i][3])
+  //     gl.drawArrays(gl.POINTS, 0, 1)
+  //   })
+  // }
+
+  let n = initVertexBuffers(gl)
+  if (n < 0) {
+    console.log('Failed to set the positions of the vertices')
+    return
   }
 
-  // function initVertexBuffers (gl) {
-  //   let vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5])
-  //   let n = 3
-  //   // 创建缓冲区对象
-  //   let vertexBuffer = gl.createBuffer()
-  //   if (!vertexBuffer) {
-  //     console.log('Failed to create the buffer object')
-  //     return -1
-  //   }
+  let xModelMatrix = new Matrix4()
+  xModelMatrix.setTranslate(0.0, 0.0, 0.0)
 
-  //   // 将缓冲区对象绑定到目标
-  //   gl.bindBuffer(gl.ARRARY_BUFFER, vertexBuffer)
+  clear(gl)
+  gl.vertexAttrib1f(aPointSize, 10.0)
+  gl.uniformMatrix4fv(uModelMatrix, false, xModelMatrix.elements)
+  // gl.uniform4f(uTranslation, 0.5, 0.5, 0.0, 0.0)
+  gl.uniform4f(uFragColor, 1.0, 0.0, 0.0, 1.0)
+  gl.drawArrays(gl.POINTS, 0, n)
 
-  //   // 向缓冲区对象中写入数据
-  //   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-  //   let aPosition = gl.getAttribLocation(gl.program, 'aPosition')
+  function initVertexBuffers (gl) {
+    let vertices = new Float32Array([0.0, 0.2, -0.2, -0.2, 0.2, -0.2])
+    let n = 3
+    // 创建缓冲区对象
+    let vertexBuffer = gl.createBuffer()
+    if (!vertexBuffer) {
+      console.log('Failed to create the buffer object')
+      return -1
+    }
 
-  //   // 连接变量与分配给它的缓冲区对象
-  //   gl.enableVertexAttrbArray(aPosition)
+    // 将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 
-  //   return n
-  // }
+    // 向缓冲区对象中写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+    let aPosition = gl.getAttribLocation(gl.program, 'aPosition')
+    // 将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0)
+
+    // 连接变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(aPosition)
+
+    let sizes = new Float32Array([10.0, 20.0, 30.0])
+    let sizeBuffer = gl.createBuffer()
+    if (!sizeBuffer) {
+      console.log('Failed to create the sizeBuffer object')
+      return -1
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW)
+    gl.vertexAttribPointer(aPointSize, 1, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(aPointSize)
+
+    return n
+  }
 }
 
 function clear (gl) {
